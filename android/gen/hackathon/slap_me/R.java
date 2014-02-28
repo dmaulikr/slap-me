fe070262c531614fde8931ae42d2804ad67148ba
elgrom <grommet@yahoo.com>
2014-02-28 07:35:05 +0000

    You can now save images based on a 1 sec timer.

--- android/gen/hackathon/slap_me/R.java
+++ android/gen/hackathon/slap_me/R.java
@@ -24,9 +24,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int imageview_main=0x7f080000;
-        public static final int surfaceview_main=0x7f080001;
+        public static final int action_settings=0x7f080001;
+        public static final int relativelayout_main=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -38,7 +37,9 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
-        public static final int take_photo=0x7f050003;
+        public static final int take_photo_back=0x7f050003;
+        public static final int take_photo_front=0x7f050004;
+        public static final int take_photo_timer=0x7f050005;
     }
     public static final class style {
         /** 
